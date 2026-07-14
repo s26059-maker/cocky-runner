d7c417221b43a7b529669f13f66e738dd4af6a44
SungWon Jung <s26059@gsm.hs.kr>
2026-07-14 22:36:26 +0900

    feat: Docker로 파이썬 코드 실행하는 러너 초기 구현

---
+++ src/main/java/com/example/cocky_runner/CockyRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocky_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockyRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockyRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cocky_runner/CockyRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cocky_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockyRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
